778f6d90c6bf4419a36b2d563e570dc64f06aa9d
Aniket <newkilling@tutanota.com>
2024-09-25 00:14:02 +0530

    First Upload

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    root: true,
+    extends: ['santdard'],
+    gloabls: {
+        'ID_DEVELOPMENT': 'randomly'
+    },
+
+    parserOptions: {
+        ecmasVersion:2020
+    }
+}
